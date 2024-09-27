0d9d368af87117f6c71a4b82b7550b420384fd02
KyleCKelly <Kyle.Kelly@student.uts.edu.au>
2024-09-27 22:36:52 +1000

    V1.1.0 with correct requirements and setup

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dashboard",
-    version="1.0.0",
+    name="CameraCountingDashboard",
+    version="1.1.0",
     description="A dashboard with people counting information including a logger and database",
     author="Kyle Kelly",
     author_email="Kyle.kelly@student.unsw.edu.au",
     packages=find_packages(),
-    include_package_data=True,
+    include_package_data=True,  # Ensures non-Python files like config/data files are included
     install_requires=[
-        'requests',
-        'sqlite3'
+        'requests',  # Add other non-standard dependencies here
     ],
     entry_points={
         'console_scripts': [
-            'dashboard = main:main',
+            'dashboard = main:main',  # Entry point for running `dashboard` from the command line
         ],
     },
+    python_requires='>=3.6',  # Adjust this if you support different versions
 )
